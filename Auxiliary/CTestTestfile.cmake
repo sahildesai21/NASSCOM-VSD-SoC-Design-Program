21a8ea954961e0ee5511cb572ea8c1c683a6cfe5
Sahil Desai <145180733+sahildesai21@users.noreply.github.com>
2024-09-30 17:58:16 +0530

    Delete Auxiliary directory

--- Auxiliary/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kunalg123/Desktop/work/tools/vsdflow/work/tools/cmake-3.13.0/Auxiliary
-# Build directory: /home/kunalg123/Desktop/work/tools/vsdflow/work/tools/cmake-3.13.0/Auxiliary
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("bash-completion")
--- Auxiliary/bash-completion/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kunalg123/Desktop/work/tools/vsdflow/work/tools/cmake-3.13.0/Auxiliary/bash-completion
-# Build directory: /home/kunalg123/Desktop/work/tools/vsdflow/work/tools/cmake-3.13.0/Auxiliary/bash-completion
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
